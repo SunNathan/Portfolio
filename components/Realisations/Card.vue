7df8312a4c35a3f84c99a4313879123c94268fa8
nathan <nathan.sunyt@gmail.com>
2025-01-27 19:11:52 +0100

    feat: add transition on hover for realisations

--- components/Realisations/Card.vue
+++ components/Realisations/Card.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="relative h-64 w-96 bg-red-200 rounded-lg overflow-hidden shadow-md">
+  <div
+      class="relative h-64 w-96 bg-red-200 rounded-lg overflow-hidden shadow-md transform transition duration-500 hover:scale-110">
     <NuxtImg src="/images/realisations/portfolio.png" class="w-full h-full object-cover"/>
-    <div class="absolute bottom-0 w-full p-4 text-center text-black bg-gradient-to-t from-[#EEF0F2] to-[rgba(238,240,242,0.70)] rounded-b-lg">
+    <div
+        class="absolute bottom-0 w-full p-4 text-center text-black bg-gradient-to-t from-[#EEF0F2] to-[rgba(238,240,242,0.70)] rounded-b-lg">
       <h3>Portfolio</h3>
     </div>
   </div>
