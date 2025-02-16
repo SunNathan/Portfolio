72b2d5af15322806e0b6e497c24ccd943fbc812f
Nathan Sun <nathan.sunyt@gmail.com>
2025-02-16 11:32:29 +0100

    fix: scroll bug with sidebar...

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex h-screen bg-[#EEF0F2]">
-    <div class="md:w-3/5 md:min-w-md 2xl:max-w-xl transition-all duration-300 overflow-x-hidden"
-         :class="{'max-md:w-screen max-md:fixed max-md:left-0 max-md:top-0': isSidebarVisible,
-         'max-md:w-0':!isSidebarVisible}">
+    <div class="md:w-3/5 md:min-w-md 2xl:max-w-xl transition-all duration-300 md:overflow-x-scroll"
+         :class="{'max-md:w-screen max-md:left-0 max-md:top-0': isSidebarVisible,
+         'max-md:w-0 max-md:hidden':!isSidebarVisible}">
       <Sidebar/>
     </div>
     <div class="m-4 w-full h-auto bg-white p-4 overflow-hidden" :class="{'max-md:hidden': isSidebarVisible}">
