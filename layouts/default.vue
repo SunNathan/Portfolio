872ac58d3490c014beb195fbb02f8be3bfacd4ba
Nathan Sun <nathan.sunyt@gmail.com>
2025-02-16 11:46:35 +0100

    feat: add mb for button

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex h-screen bg-[#EEF0F2]">
+  <div class="flex h-screen bg-[#EEF0F2] mb-16">
     <div class="md:w-3/5 md:min-w-md 2xl:max-w-xl transition-all duration-300 md:overflow-x-scroll"
          :class="{'max-md:w-screen max-md:left-0 max-md:top-0': isSidebarVisible,
          'max-md:w-0 max-md:hidden':!isSidebarVisible}">
